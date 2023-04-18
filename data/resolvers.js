5014e5b543da9b98248585edd6e56d825ff230f1
Pete Tian <pete.tian@gmail.com>
2023-04-17 17:09:16 -0700

    change update function

--- data/resolvers.js
+++ data/resolvers.js
@@ -33,10 +33,17 @@ const resolvers = {
 
     // function to update a product
     updateProduct: ({id, input}) => {
-        console.log("Input received:", input); // Add this line for debugging
-        const newProduct = new Product(id, input);
-        productDatabase[id] = newProduct;
-        return newProduct;
+        console.log("Update received:", input); // Add this line for debugging
+
+        // if the product does not exist, send an error
+        if (!productDatabase[id]) {
+            throw new Error('No product exists with id ' + id);
+        }
+
+        // find the product by id then update
+        const updatedProduct = new Product(id, input);  
+        productDatabase[id] = updatedProduct;
+        return updatedProduct;
     },
 
     // function to delete a product
